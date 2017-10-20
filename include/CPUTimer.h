3051d4a1a829d30db4be7ee4e17579f7a3ac6423
Adrian Haarbach <mail@adrian-haarbach.de>
2017-10-20 16:16:49 +0200

    CPUTimer using C++11 chrono for platform independence, added dirent.h dropin for easy windows filesystem access

--- include/CPUTimer.h
+++ include/CPUTimer.h
@@ -4,48 +4,18 @@
 #include <iostream>
 #include <string>
 #include <map>
-#include <time.h>
+#include <chrono>
 
-#ifndef CLOCK_REALTIME
-  #define CLOCK_REALTIME CLOCK_MONOTONIC
-#endif
-
-#ifdef __MACH__
-#include <mach/clock.h>
-#include <mach/mach.h>
-#endif
-
-#ifdef _WIN32
-#include <wintime.h>
-#else
-#include <sys/time.h>
-#endif
+typedef std::chrono::steady_clock Clock;
 
 class CPUTimer {
  private:
-  timespec startTime;
-  timespec endTime;
-
-  bool startWasSet;
-  bool stopWasSet;
-
-  void getTime(struct timespec *ts);
-
+  Clock::time_point startTime;
   std::map<std::string,float> timingsMap;
 
  public:
-  CPUTimer();
-  ~CPUTimer();
-
   void tic();
-  timespec toc();
   void toc(std::string name);
-
-  float tocSeconds();
-
   void printAllTimings();
-
-  std::string getHeader();
-  std::string getMeasurements();
 };
 #endif
--- src/internal/CPUTimer.cpp
+++ src/internal/CPUTimer.cpp
@@ -9,76 +9,20 @@
 
 using namespace std;
 
-CPUTimer::CPUTimer() : startWasSet(false), stopWasSet(true)  {
-}
-
-CPUTimer::~CPUTimer() {
-}
-
-void CPUTimer::getTime(struct timespec *ts){
-#ifdef __MACH__ 
-  //https://gist.github.com/jbenet/1087739
-  // OS X does not have clock_gettime, use clock_get_time
-  clock_serv_t cclock;
-  mach_timespec_t mts;
-  host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
-  clock_get_time(cclock, &mts);
-  mach_port_deallocate(mach_task_self(), cclock);
-  ts->tv_sec = mts.tv_sec;
-  ts->tv_nsec = mts.tv_nsec;
-#else
-#ifdef _WIN32
-  clock_gettime(ts);
-#else
-  clock_gettime(CLOCK_REALTIME, ts);
-#endif
-#endif
-
-}
-
 void CPUTimer::tic() {
-  assert(stopWasSet && !startWasSet);
-
-  getTime(&startTime);
-  startWasSet = true;
-  stopWasSet = false;
-}
-
-timespec CPUTimer::toc() {
-  assert(startWasSet && !stopWasSet);
-  getTime(&endTime);
-
-  timespec temp;
-
-  if ((endTime.tv_nsec - startTime.tv_nsec)<0) {
-    temp.tv_sec = endTime.tv_sec - startTime.tv_sec-1;
-    temp.tv_nsec = 1000000000 + endTime.tv_nsec - startTime.tv_nsec;
-  } else {
-    temp.tv_sec = endTime.tv_sec - startTime.tv_sec;
-    temp.tv_nsec = endTime.tv_nsec - startTime.tv_nsec;
-  }
-
-  stopWasSet = true;
-  startWasSet = false;
-
-  return temp;
-}
-
-float CPUTimer::tocSeconds(){
-    timespec elapsedTimeCPUTimer = this->toc();
-    return elapsedTimeCPUTimer.tv_sec + elapsedTimeCPUTimer.tv_nsec / 1e9f;
+  startTime = Clock::now();
 }
 
 void CPUTimer::toc(std::string name){
-  timespec elapsedTimeCPUTimer = this->toc();
+  Clock::time_point endTime = Clock::now();
+  auto s = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime);
+  double seconds = s.count() * 1e-6f; //1e6 microseconds is 1s
   cout<<endl;
   cout<<"=====  TIMING["<<name<<"] is ";
-  cout<< elapsedTimeCPUTimer.tv_sec << "." << elapsedTimeCPUTimer.tv_nsec << " s" << endl;          
+  cout<< seconds << " s" << endl;          
   cout << endl;
 
-  float seconds=elapsedTimeCPUTimer.tv_sec + elapsedTimeCPUTimer.tv_nsec / 1e9f;
-
-  timingsMap[name]=seconds;
+  timingsMap[name] = seconds;
 }
 
 void CPUTimer::printAllTimings(){
@@ -89,24 +33,4 @@ void CPUTimer::printAllTimings(){
     cout<< left << setw(20) << it->first<<":\t";
     printf("%0.3f\n",it->second);
   }
-}
-
-string CPUTimer::getHeader(){
-    typedef std::map<std::string,float>::iterator it_type;
-    std::stringstream ss;
-
-    for(it_type it = timingsMap.begin(); it != timingsMap.end(); ++it) {
-        ss<<","<<it->first;
-    }
-    return ss.str();
-}
-
-string CPUTimer::getMeasurements(){
-    typedef std::map<std::string,float>::iterator it_type;
-    std::stringstream ss;
-
-    for(it_type it = timingsMap.begin(); it != timingsMap.end(); ++it) {
-        ss<<","<<it->second;
-    }
-    return ss.str();
 }
