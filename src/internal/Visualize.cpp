bb94fbccfbe1993a5b06fb2801e3b2f9455592bc
Adrian Haarbach <mail@adrian-haarbach.de>
2018-03-14 10:59:02 +0100

    fix: GLFW_OPENGL_COMPAT_PROFILE instead of GLFW_OPENGL_CORE_PROFILE to make matrix stack work again so we see something in ./multiview

--- src/internal/Visualize.cpp
+++ src/internal/Visualize.cpp
@@ -70,7 +70,7 @@ Visualize::Visualize()
     if (!glfwInit()) exit(-1);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
     window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "GLFW PointCloud Viewer", NULL, NULL);
